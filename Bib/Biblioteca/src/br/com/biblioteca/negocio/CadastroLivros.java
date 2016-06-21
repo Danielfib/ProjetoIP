4de188b2c8a1f929bd7601f6f30e01ac11e556b7
Mateusnunes <mateununes@me.com>
2016-06-20 23:19:46 -0300

    Criei cadastroLivros

--- Bib/Biblioteca/src/br/com/biblioteca/negocio/CadastroLivros.java
+++ Bib/Biblioteca/src/br/com/biblioteca/negocio/CadastroLivros.java
@@ -1,4 +1,30 @@
+package br.com.biblioteca.negocio;
+
+
+import br.com.biblioteca.basic.Livro;
+import br.com.biblioteca.interfaces.RepositorioLivro;
 
 public class CadastroLivros {
 
+	private RepositorioLivros livros;
+	
+	public CadastroLivro (RepositorioLivro novo) {
+		this.Livros = novo;
+	}
+	
+	public void cadastrar(Livro e) {
+
+	}
+	
+	public void remover(Livro e) {
+		
+	}
+	
+	public void atualizar(Livro e) {
+		//nao feito no repositorioLivroLISTA NEM ARRAY***
+	}
+	
+	public Livro procurar(Livro l) {
+		return null;
+	}
 }
