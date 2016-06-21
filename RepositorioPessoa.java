315f7d8ba9b548ef8b5dc78348e6157ed0ce4a98
Danielfib <dfb2@cin.ufpe.br>
2016-06-20 22:39:50 -0300

    interface e repositorio de array de pessoas
    s2

---
+++ RepositorioPessoa.java
@@ -0,0 +1,12 @@
+
+public interface RepositorioPessoa {	
+    public void inserir(Pessoa pessoa);	
+    public Pessoa procurar (Pessoa pessoa);
+	public void remover (Pessoa pessoa);
+	public void atualizar(Pessoa pessoa);	
+
+}
+
+
+
+
