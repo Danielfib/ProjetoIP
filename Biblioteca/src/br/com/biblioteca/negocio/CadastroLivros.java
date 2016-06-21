d13e88bd42ab1a0cb0e68d9b34b089ebe95d1fc9
Mateusnunes <mateusnunes@me.com>
2016-06-21 18:13:25 -0300

    em andamento

--- Biblioteca/src/br/com/biblioteca/negocio/CadastroLivros.java
+++ Biblioteca/src/br/com/biblioteca/negocio/CadastroLivros.java
@@ -1,4 +1,37 @@
 package br.com.biblioteca.negocio;
+
+
+import br.com.biblioteca.basic.Livro;
+import br.com.biblioteca.interfaces.RepositorioLivro;
+
 public class CadastroLivros {
 
+	private RepositorioLivro novoLivro;
+	
+	public CadastroLivros(RepositorioLivro livro){
+		this.novoLivro = livro;
+		
+	}
+	
+	public void cadastrar(Livro livro){
+		if (this.novoLivro.procurar(livro.getTitulo()) == null){
+			this.novoLivro.inserir(livro);
+		}else{
+			//exception
+		}
+	}
+	public void remover(Livro livro){
+		if (this.novoLivro.procurar(livro.getTitulo()) != null){
+			this.novoLivro.remover(livro);
+		}else{
+			//exception
+		}
+	}
+	public void atualizar(Livro livro){
+		if(novoLivro.atualizar(livro)==null){
+			//execpiton
+		}
+	}
+	
+	
 }
