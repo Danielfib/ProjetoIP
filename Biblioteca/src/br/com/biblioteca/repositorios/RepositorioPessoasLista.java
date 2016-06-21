c45e2138d2a292d6a00123544ed646b104930a26
Danielfib <dfb2@cin.ufpe.br>
2016-06-21 15:55:53 -0300

    vai flavio

---
+++ Biblioteca/src/br/com/biblioteca/repositorios/RepositorioPessoasLista.java
@@ -0,0 +1,63 @@
+package br.com.biblioteca.repositorios;
+
+import br.com.biblioteca.basic.Node;
+import br.com.biblioteca.basic.Pessoa;
+import br.com.biblioteca.interfaces.RepositorioPessoa;
+
+public class RepositorioPessoasLista implements RepositorioPessoa {
+
+	public Node first;
+
+	public void criarLista() {
+		first = null;
+	}
+
+	@Override
+	public void inserir(Pessoa pessoa) {
+		Node primeiro = first;
+		if (first == null){
+			first = new Node(pessoa);
+		} else {
+			while (primeiro.getProximo() != null){
+				primeiro = primeiro.getProximo(); //vai ate o ultimo da lista
+			}			
+			primeiro.setProximo(new Node(pessoa));
+		}
+	}
+
+	@Override
+	public Pessoa procurar(Pessoa pessoa) {
+		for (Node node = first; node != null; node = node.getProximo()){
+			if (node.getPessoa() == pessoa){
+				return pessoa;
+			}
+		}
+		return null;		
+	}
+
+	@Override
+	public void remover(Pessoa pessoa) {
+		Node prev = null;
+		Node prim = first;
+		
+		while (prim != null && prim.getPessoa() != pessoa){
+			prev = prim;
+			prim = prim.getProximo();
+		}
+		if (prim==null){
+			return;
+		}
+		if (prev == null){
+			first = prim.getProximo(); 
+		} else {
+			prev.setProximo(prim.getProximo());
+		}
+	}
+
+	@Override
+	public void atualizar(Pessoa pessoa) {
+		
+
+	}
+
+}
