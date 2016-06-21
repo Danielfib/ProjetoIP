7a8376dedb976c43797b9fcccfb18658f73b4d78
Danielfib <dfb2@cin.ufpe.br>
2016-06-20 22:46:56 -0300

    Update RepositorioPessoa.java

--- RepositorioPessoa.java
+++ RepositorioPessoa.java
@@ -2,8 +2,8 @@
 public interface RepositorioPessoa {	
     public void inserir(Pessoa pessoa);	
     public Pessoa procurar (Pessoa pessoa);
-	public void remover (Pessoa pessoa);
-	public void atualizar(Pessoa pessoa);	
+    public void remover (Pessoa pessoa);
+    public void atualizar(Pessoa pessoa);	
 
 }
 
