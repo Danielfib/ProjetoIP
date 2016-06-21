911fecb2228ad597003be30ee5f25e8e94e699bb
Ramom Pereira <r.ieadpe@gmail.com>
2016-06-21 18:48:33 -0300

    Exceptions Melhoradas

--- Biblioteca/src/br/com/biblioteca/negocio/CadastroEmprestimo.java
+++ Biblioteca/src/br/com/biblioteca/negocio/CadastroEmprestimo.java
@@ -3,10 +3,8 @@ package br.com.biblioteca.negocio;
 import br.com.biblioteca.basic.Aluno;
 import br.com.biblioteca.basic.Emprestimo;
 import br.com.biblioteca.basic.Livro;
-import br.com.biblioteca.exceptions.EmprestimoExistenteException;
-import br.com.biblioteca.exceptions.EmprestimoNaoEncontradoException;
+import br.com.biblioteca.exceptions.EmprestimoException;
 import br.com.biblioteca.interfaces.RepositorioEmprestimos;
-import br.com.biblioteca.repositorios.RepositorioEmprestimosArray;
 
 public class CadastroEmprestimo {
 
@@ -16,32 +14,32 @@ public class CadastroEmprestimo {
 		this.emprestimos = emp;
 	}
 	
-	public void cadastrar(Emprestimo e) throws EmprestimoExistenteException {
+	public void cadastrar(Emprestimo e) throws EmprestimoException {
 		if(emprestimos.procurar(e.getLivro(), e.getAluno()) == null) {
 			emprestimos.inserir(e);
 		} else {
-			throw new EmprestimoExistenteException();
+			throw new EmprestimoException("Empréstimo Existente!");
 		}
 	}
 	
-	public void remover(Emprestimo e) throws EmprestimoNaoEncontradoException {
+	public void remover(Emprestimo e) throws EmprestimoException {
 		if(emprestimos.procurar(e.getLivro(), e.getAluno()) != null) {
 			emprestimos.remover(e);
 		} else {
-			throw new EmprestimoNaoEncontradoException();
+			throw new EmprestimoException("Empréstimo Inexistente!");
 		}
 	}
 	
-	public void atualizar(Emprestimo e) throws EmprestimoNaoEncontradoException {
+	public void atualizar(Emprestimo e) throws EmprestimoException {
 		if(emprestimos.atualizar(e) == null) {
-			throw new EmprestimoNaoEncontradoException();
+			throw new EmprestimoException("Empréstimo Inexistente!");
 		}
 	}
 	
-	public Emprestimo procurar(Livro l, Aluno a) throws EmprestimoNaoEncontradoException {
+	public Emprestimo procurar(Livro l, Aluno a) throws EmprestimoException {
 		Emprestimo emp = emprestimos.procurar(l, a);
 		if(emp == null) {
-			throw new EmprestimoNaoEncontradoException();
+			throw new EmprestimoException("Empréstimo Inexistente!");
 		}
 		return emp;
 	}
---
+++ Biblioteca/src/br/com/biblioteca/principal/Biblioteca.java
@@ -0,0 +1,13 @@
+package br.com.biblioteca.principal;
+
+import br.com.biblioteca.negocio.CadastroEmprestimo;
+import br.com.biblioteca.negocio.CadastroEspacoEstudo;
+import br.com.biblioteca.negocio.CadastroLivros;
+
+public class Biblioteca {
+
+	CadastroEmprestimo cadastroEmprestimo;
+	CadastroEspacoEstudo cadastroEspEstudo;
+	CadastroLivros cadastroLivro;
+	
+}
