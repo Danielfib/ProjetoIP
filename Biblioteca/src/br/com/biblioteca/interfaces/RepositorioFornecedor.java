1f34642352271bb08717a753ad8c610a971d215b
Mateusnunes <mateusnunes@me.com>
2016-06-21 22:37:18 -0300

    Atualização repositorio

--- Biblioteca/src/br/com/biblioteca/interfaces/RepositorioFornecedor.java
+++ Biblioteca/src/br/com/biblioteca/interfaces/RepositorioFornecedor.java
@@ -6,7 +6,7 @@ import br.com.biblioteca.basic.Quadrado1;
 public interface RepositorioFornecedor {
 
 	void inserir(Fornecedor fornecedor);
-	public Fornecedor procurar(int cnpj );
+	Fornecedor procurar(int cnpj );
 	void remover(Fornecedor fornecedor);
 	Fornecedor atualizar(Fornecedor fornecedor);
 }
--- Biblioteca/src/br/com/biblioteca/interfaces/RepositorioLivro.java
+++ Biblioteca/src/br/com/biblioteca/interfaces/RepositorioLivro.java
@@ -6,7 +6,7 @@ import br.com.biblioteca.basic.Quadrado;
 public interface RepositorioLivro {
 
 	 void inserir(Livro livro);
-	 public Livro procurar(String titulo);
+	 Livro procurar(String titulo);
 	 void remover(Livro livro);
 	 Livro atualizar(Livro livro);
 
