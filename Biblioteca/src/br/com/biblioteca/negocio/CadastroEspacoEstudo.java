b8163dc2c1d141f530eb3574165cb2be4b97758e
NNeto12345 <nesn@cin.ufpe.br>
2016-06-21 23:20:32 -0300

    CadastroEspacoEstudo

--- Biblioteca/src/br/com/biblioteca/negocio/CadastroEspacoEstudo.java
+++ Biblioteca/src/br/com/biblioteca/negocio/CadastroEspacoEstudo.java
@@ -3,6 +3,7 @@ package br.com.biblioteca.negocio;
 import br.com.biblioteca.basic.Aluno;
 import br.com.biblioteca.basic.EspacoEstudo;
 import br.com.biblioteca.interfaces.RepositorioEspacoEstudo;
+import br.com.biblioteca.exceptions.EspacoEstudoException;
 
 public class CadastroEspacoEstudo {
 	
@@ -12,16 +13,27 @@ public class CadastroEspacoEstudo {
 		this.espacosEstudo = espaco;
 	}
 	
-	public void cadastrar(EspacoEstudo espaco){
+	public void cadastrar(EspacoEstudo espaco) throws EspacoEstudoException{
+		if(this.espacosEstudo.procurar(espaco.getIdentificador()) == null){
+			this.espacosEstudo.inserir(espaco);
+		}else{
+			throw new EspacoEstudoException("Espaco ja existente");
+		}
 		
 	}
-	public void remover (EspacoEstudo espaco){
-		
+	public void remover (EspacoEstudo espaco) throws EspacoEstudoException{
+		if(this.espacosEstudo.procurar(espaco.getIdentificador()) != null){
+			this.espacosEstudo.remover(espaco);
+		}else{
+			throw new EspacoEstudoException("Espaco inexistente");
+		}
 	}
-	public void atualizar (EspacoEstudo espaco){
-		
+	public void atualizar (EspacoEstudo espaco) throws EspacoEstudoException{
+		if(espacosEstudo.atualizar(espaco) == null){
+			throw new EspacoEstudoException("Espaco inexistente");
+		}
 	}
 	public EspacoEstudo procurar(int identificador){
-		return null;
+		return espacosEstudo.procurar(identificador);
 	}
 }
